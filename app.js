f0e33ac530c08f2a8d6c1b83e3c97add01975fcb
stevengum97 <steven.gum@gmail.com>
2017-04-27 14:32:59 -0700

    creation of simple restify bot template, botbuilder=^3.7.0

---
+++ app.js
@@ -0,0 +1,18 @@
+var builder = require('botbuilder'); 
+var restify = require('restify');
+require('dotenv').config();
+
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+server.post('/api/messages', connector.listen());
+
+var bot = new builder.UniversalBot(connector);
+
